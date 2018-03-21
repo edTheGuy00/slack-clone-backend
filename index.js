2d962ac020a5d7c30988085c0d52c06cca0be38b
edgar <edgrr.trem000@gmail.com>
2018-03-21 16:06:45 -0500

    server running

--- index.js
+++ index.js
@@ -3,22 +3,43 @@ import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
 
-import typeDefs from './schema';
-import resolvers from './resolvers'
+import path from 'path';
+import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
+import cors from 'cors';
 
-export const schema = makeExecutableSchema({
+import models from './models';
+
+const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
+const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')));
+
+const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
 });
 
 
 const app = express();
+app.use(cors('*'));
 
 const graphqlEndPoint = '/graphql';
 
 // bodyParser is needed just for POST.
-app.use(graphqlEndPoint, bodyParser.json(), graphqlExpress({ schema: schema }));
+app.use(
+  graphqlEndPoint,
+  bodyParser.json(),
+  graphqlExpress({
+    schema,
+    context: {
+      models,
+      user: {
+        id: 1,
+      },
+    },
+  }),
+);
 
 app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndPoint }));
 
-app.listen(8080);
+models.sequelize.sync().then(() => {
+  app.listen(8080);
+});
