c1dd284b9dee3b0c38a76aabf5af4fd10f4a6367
edgar <edgrr.trem000@gmail.com>
2018-03-23 10:56:40 -0500

    refactor auth and login

--- models/user.js
+++ models/user.js
@@ -1,31 +1,52 @@
+import bcrypt from 'bcrypt';
+
 export default (sequelize, DataTypes) => {
-  const User = sequelize.define('user', {
-    username: {
-      type: DataTypes.STRING,
-      unique: true,
-      validate: {
-        isAlphanumeric: {
-          args: true,
-          msg: 'The username can only contain numbers and letters',
+  const User = sequelize.define(
+    'user', {
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+          isAlphanumeric: {
+            args: true,
+            msg: 'The username can only contain numbers and letters',
+          },
+          len: {
+            args: [3, 25],
+            msg: 'The username needs to be between 3 and 25 characters long',
+          },
         },
-        len: {
-          args: [3, 25],
-          msg: 'The username needs to be between 3 and 25 characters long',
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: 'Invalid email',
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [5, 100],
+            msg: 'The password needs to be at least 5 characters long',
+          },
         },
       },
     },
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      validate: {
-        isEmail: {
-          args: true,
-          msg: 'Invalid email',
+    {
+      hooks: {
+        afterValidate: async (user) => {
+          const hashedPassword = await bcrypt.hash(user.password, 12);
+          // eslint-disable-next-line
+          user.password = hashedPassword;
         },
       },
     },
-    password: DataTypes.STRING,
-  });
+  );
 
   User.associate = (models) => {
     User.belongsToMany(models.Team, {
