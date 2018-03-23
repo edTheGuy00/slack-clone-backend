940acca0c6a5959aaacea1e81a81b09eef50e2a4
edgar <edgrr.trem000@gmail.com>
2018-03-23 11:57:11 -0500

    fix eslint errors

--- resolvers/channel.js
+++ resolvers/channel.js
@@ -1,13 +1,13 @@
 export default {
-    Mutation: {
-        createChannel: async (parent, args, {models}) => {
-            try {
-                await models.Channel.create(args)
-                return true;
-            } catch (error) {
-                console.log(error)
-                return false;
-            }
-        }
-    }
+  Mutation: {
+    createChannel: async (parent, args, { models }) => {
+      try {
+        await models.Channel.create(args);
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
 };
