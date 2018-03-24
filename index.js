3f82f5d25431aaef9eac87a0703af74bdb569989
edgar <edgrr.trem000@gmail.com>
2018-03-24 18:52:23 -0500

    send, verify, and refresh auth token

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
-
+import jwt from 'jsonwebtoken';
 import path from 'path';
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 import cors from 'cors';
 
+
+import { refreshTokens } from './auth';
 import models from './models';
 
 const SECRET = 'jcw98sdnuvuhdv8sdijl';
@@ -24,23 +26,44 @@ const schema = makeExecutableSchema({
 const app = express();
 app.use(cors('*'));
 
+const addUser = async (req, res, next) => {
+  const token = req.headers['x-token'];
+
+  if (token) {
+    try {
+      const { user } = jwt.verify(token, SECRET);
+      req.user = user;
+    } catch (error) {
+      const refreshToken = req.headers['x-refresh-token'];
+      const newTokens = await refreshTokens(token, refreshToken, models, SECRET, SECRET2);
+      if (newTokens.token && newTokens.refreshToken) {
+        res.set('Access-Control-Expose-Headers', 'x-token, x-refresh-token');
+        res.set('x-token', newTokens.token);
+        res.set('x-refresh-token', newTokens.refreshToken);
+      }
+      req.user = newTokens.user;
+    }
+  }
+  next();
+};
+
+app.use(addUser);
+
 const graphqlEndPoint = '/graphql';
 
 // bodyParser is needed just for POST.
 app.use(
   graphqlEndPoint,
   bodyParser.json(),
-  graphqlExpress({
+  graphqlExpress(req => ({
     schema,
     context: {
       models,
-      user: {
-        id: 1,
-      },
+      user: req.user,
       SECRET,
       SECRET2,
     },
-  }),
+  })),
 );
 
 app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndPoint }));
