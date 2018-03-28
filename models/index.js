7f10794f964f9deb8b14b9a8cf4f8de8937d682a
edgar <edgrr.trem000@gmail.com>
2018-03-27 21:07:05 -0500

    fix eslint errors

--- models/index.js
+++ models/index.js
@@ -1,21 +1,20 @@
-import Sequelize from "sequelize";
+import Sequelize from 'sequelize';
 
 const sequelize = new Sequelize('slack', 'postgres', 'postgres', {
-    dialect: 'postgres',
-    define: {
-      underscored: true,
-    }
-    
+  dialect: 'postgres',
+  define: {
+    underscored: true,
+  },
 });
 
 const models = {
-    User: sequelize.import('./user'),
-    Channel: sequelize.import('./channel'),
-    Team: sequelize.import('./team'),
-    Message: sequelize.import('./message')
+  User: sequelize.import('./user'),
+  Channel: sequelize.import('./channel'),
+  Team: sequelize.import('./team'),
+  Message: sequelize.import('./message'),
 };
 
-Object.keys(models).forEach(modelName => {
+Object.keys(models).forEach((modelName) => {
   if (models[modelName].associate) {
     models[modelName].associate(models);
   }
