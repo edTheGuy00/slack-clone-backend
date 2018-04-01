f7363ee1a5a9a1a0442fc781ffa3cfa857c3330b
edgar <edgrr.trem000@gmail.com>
2018-04-01 16:29:13 -0500

    include team invites query

--- resolvers/team.js
+++ resolvers/team.js
@@ -5,6 +5,16 @@ export default {
   Query: {
     allTeams: requiresAuth.createResolver(async (parent, args, { models, user }) =>
       models.Team.findAll({ where: { owner: user.id } }, { raw: true })),
+
+      teamInvites: requiresAuth.createResolver(async (parent, args, { models, user }) =>
+      models.Team.findAll({
+        include: [
+          {
+            model: models.User,
+            where: { id: user.id },
+          },
+        ],
+      }, { raw: true })),
   },
 
   Mutation: {
--- schema/team.js
+++ schema/team.js
@@ -16,6 +16,7 @@ export default `
 
     type Query {
         allTeams: [Team!]!
+        teamInvites: [Team!]!
     }
 
     type VoidResponse {
