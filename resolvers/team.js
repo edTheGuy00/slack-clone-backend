de564c8a0a910eeb8be97dd94bc9fc7fdb1212d8
edgar <edgrr.trem000@gmail.com>
2018-03-31 23:08:55 -0500

    create join table for users and teams

--- resolvers/team.js
+++ resolvers/team.js
@@ -8,6 +8,35 @@ export default {
   },
 
   Mutation: {
+    addTeamMember: requiresAuth.createResolver(async (parent, { email, teamId }, { models, user }) => {
+      try {
+        const teamPromise = await models.Team.findOne({ where: { id: teamId } }, { raw: true });
+        const userToAddPromise = await models.User.findOne({ where: { email } }, { raw: true });
+        const [team, userToAdd] = await Promise.all([teamPromise, userToAddPromise]);
+        if (team.owner !== user.id) {
+          return {
+            ok: false,
+            errors: [{ path: 'email', message: 'You cannot add members to the team' }],
+          };
+        }
+        if (!userToAdd) {
+          return {
+            ok: false,
+            errors: [{ path: 'email', message: 'Could not find user with this email' }],
+          };
+        }
+        await models.Member.create({ userId: userToAdd.id, teamId });
+        return {
+          ok: true,
+        }
+      } catch (err) {
+        console.log(err);
+        return {
+          ok: false,
+          errors: formatErrors(err, models),
+        };
+      }
+    }),
     createTeam: requiresAuth.createResolver(async (parent, args, { models, user }) => {
       try {
         const team = await models.Team.create({ ...args, owner: user.id });
